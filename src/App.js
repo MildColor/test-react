306b492e55122c634eb86871fb5b487a29803a58
MildColor <96479626+MildColor@users.noreply.github.com>
2022-10-05 14:44:49 +0900

    react-router-dom example2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import Mother from "./components/Test/test";
-import "./App.css";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Detail from "./components/Test/Detail";
+import Home from "./components/Test/Home";
 function App() {
-  return <Mother></Mother>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/movie" element={<Detail />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
+
+// react-router-dom
+// npm i react-router-dom@5.3.0
+// https://v5.reactrouter.com/web/guides/quick-start
+
+// react-router-dom 5버전 -> 버전6 바뀐 부분
+
+// 1. Switch컴포넌트가 Routes컴포넌트로 대체되었습니다.
+// Switch -> Routes
+
+// 2. Route컴포넌트 사이에 자식 컴포넌트를 넣지 않고, element prop에 자식 컴포넌트를 할당하도록 바뀌었습니다.
+// Route path="/" element={< Home / >}
+
+// react-router-dom 6버전 문서
+// https://reactrouter.com/docs/en/v6/getting-started/overview
