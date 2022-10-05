dad60456a58124dc5c3d9d99427cb2e8b6e7a142
MildColor <96479626+MildColor@users.noreply.github.com>
2022-10-05 12:43:29 +0900

    button show and hide

--- src/components/Test/test.jsx
+++ src/components/Test/test.jsx
@@ -1,45 +1,20 @@
 import { useState, useEffect } from "react";
 
-function Mother() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-
-  console.log("i run all the time");
-
-  // 첫번째 렌더에만 실행
+function Hello() {
   useEffect(() => {
-    console.log("I run only once");
+    console.log("I'm here");
   }, []);
+  return <h1>Hello</h1>;
+}
 
-  useEffect(() => {
-    console.log("I run when 'counter' and 'keyword' changes");
-  }, [keyword, counter]);
-
+function Mother() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        type="text"
-        onChange={onChange}
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
 export default Mother;
-// 지금은 클릭할 때마다 계속 render가 되는 중
-// 가끔은 첫번째로 render할 때만 코드를 실행하고 싶을 수도 있다.
-// 그래서 첫번째 render에만 코드가 실행되고, 다른 state변화에는 실행되지 않도록 하는것.
-// ex) API를 가져올 때, 첫번째 component render에서 API를 콜하고, 이후에 state가 변할 때, 그 API에서 데이터를 또다시 가져오고 싶진 않을 것이다.
-
-// • 리액트를 사용하는 이유: 최소 단위의 렌더링을 위해
-// • useState(): 변수, 변수를 제어하는 함수로 구성되며 변하는 값을 제어, 해당 부분의 리렌더링을 위함
-// • props: 태그의 속성 값을 함수의 아규먼트 처럼 컴포넌트에 값을 전달해준다.
-// • useEffect(): 코드의 실행 시점을 관리할 수 있는 선택권을 얻는 방어막 같은 존재, 디펜던시가 없을 경우 최초 1회 실행, 있을 경우 해당 값이 변할 경우 실행한다. 이 때 디펜던시는 여러개 입력이 가능하다.
-
-// 🏴 부모 컴포넌트에서 리렌더링이 일어날 경우 모든 자식들이 리렌더링이 된다.(wa can use memo)
-// 🏴 propType을 설치하고 props의 타입을 지정해 줄 수 있다. 이 때 isRequired로 필수값을 지정 가능
