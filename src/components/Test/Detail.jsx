78531b706427bd956c1932a8089236da36793ea0
MildColor <96479626+MildColor@users.noreply.github.com>
2022-10-05 16:07:06 +0900

    parameter

--- src/components/Test/Detail.jsx
+++ src/components/Test/Detail.jsx
@@ -1,4 +1,18 @@
+// 현재 위치하고 있는 url에서 parameter를 받기 위함
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
+  const { id } = useParams();
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    console.log(json);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
   return <h1>Detail</h1>;
 }
 export default Detail;
--- src/components/Test/Movie.jsx
+++ src/components/Test/Movie.jsx
@@ -1,12 +1,12 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-function Movie({ coverImg, title, summary, genres }) {
+function Movie({ coverImg, title, summary, genres, id }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
       <h2>
-        <Link to="/movie">{title}</Link>
+        <Link to={`/movie/${id}`}>{title}</Link>
       </h2>
       <p>{summary}</p>
       <ul>
@@ -19,6 +19,7 @@ function Movie({ coverImg, title, summary, genres }) {
 }
 
 Movie.propTypes = {
+  id: PropTypes.number.isRequired,
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
@@ -26,3 +27,5 @@ Movie.propTypes = {
 };
 
 export default Movie;
+
+// 일반 html에서는 a태그에 href를 썼겠지만, react에서는 Link to= "/"를 이용한다.
