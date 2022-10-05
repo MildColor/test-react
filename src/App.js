fb9960ecc68b9a8b7e9efa7d364f6bf1ca7d2b58
MildColor <96479626+MildColor@users.noreply.github.com>
2022-10-05 17:12:59 +0900

    redux connecting the store

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Detail from "./components/Test/Detail";
-import Home from "./components/Test/Home";
+import React from "react";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./routers/Home";
+import Detail from "./routers/Detail";
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/movie/:id" element={<Detail />}></Route>
+        <Route path="/:id" element={<Detail />}></Route>
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
-// react-router-dom
-// npm i react-router-dom@5.3.0
-// https://v5.reactrouter.com/web/guides/quick-start
-
-// react-router-dom 5버전 -> 버전6 바뀐 부분
-
-// 1. Switch컴포넌트가 Routes컴포넌트로 대체되었습니다.
-// Switch -> Routes
-
-// 2. Route컴포넌트 사이에 자식 컴포넌트를 넣지 않고, element prop에 자식 컴포넌트를 할당하도록 바뀌었습니다.
-// Route path="/" element={< Home / >}
-
-// react-router-dom 6버전 문서
-// https://reactrouter.com/docs/en/v6/getting-started/overview
